fc52647ab9e90bf9ca20f37ac905c80337ee4c72
冯伟棋 <fengweiqi@365huaer.net>
2022-08-04 18:34:55 +0800

    chore: 根据环境拆分打包配置, 调整首页样式

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,16 +3,6 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 // 生成html
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-// 分离包中的css代码
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// 构建结果分析
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-// 压缩css
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-// 内置压缩js插件
-// eslint-disable-next-line import/no-extraneous-dependencies
-const TerserPlugin = require('terser-webpack-plugin');
 // vue加载器
 const { VueLoaderPlugin } = require('vue-loader');
 // ESlint-webpack插件
@@ -21,7 +11,6 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 console.log('process.env.NODE_ENV =', process.env.NODE_ENV); // 打印环境变量
 
 module.exports = {
-  target: 'web',
   // 入口
   entry: './src/main.js',
   // 输出
@@ -31,6 +20,19 @@ module.exports = {
     chunkFilename: 'assets/js/[name].js',
     path: path.join(__dirname, 'dist'), // 输出文件目录
     clean: true, // 清理dist
+  },
+  // 配置映射关系
+  devtool: 'source-map',
+  // 本地开发服务
+  devServer: {
+    // 本地端口号设置
+    port: 8585,
+    // 静态资源本地访问配置
+    static: {
+      directory: path.join(__dirname, 'src/assets'),
+      publicPath: '/src/assets', // 告诉服务器在哪个 URL 上提供 static.directory 的内容
+      watch: true,
+    },
   },
 
   resolve: {
@@ -59,7 +61,7 @@ module.exports = {
       {
         test: /\.(s[ac]|c)ss$/i, // 匹配所有的 css 文件
         use: [
-          MiniCssExtractPlugin.loader,
+          'vue-style-loader',
           'css-loader',
           'postcss-loader',
           'sass-loader',
@@ -102,63 +104,16 @@ module.exports = {
   // 配置插件
   plugins: [
     new Dotenv({
-      path: './.env.production',
+      path: './.env.development',
       systemvars: true, // 允許讀取 process.env 下的任意系統變量
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/webpack.ico',
     }),
-    new MiniCssExtractPlugin({
-      filename: 'assets/style/[name].[hash:8].css',
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'server', // 不启动展示打包报告的http服务器
-      // generateStatsFile: true, // 是否生成stats.json文件
-    }),
     new VueLoaderPlugin(),
     new ESLintPlugin(),
   ],
-
-  // 分包优化
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        // default: false,
-        styles: {
-          name: 'chunk-styles',
-          test: /\.(s?css|less|sass)$/,
-          chunks: 'all',
-          enforce: true,
-          priority: 10,
-        },
-        common: {
-          name: 'chunk-common',
-          chunks: 'all',
-          minChunks: 2,
-          maxInitialRequests: 5,
-          minSize: 0,
-          priority: 1,
-          enforce: true,
-          reuseExistingChunk: true,
-        },
-        vendors: {
-          name: 'chunk-vendors',
-          test: /[\\/]node_modules[\\/]/,
-          chunks: 'all',
-          priority: 2,
-          enforce: true,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    minimize: true,
-    minimizer: [
-      // 添加 css 压缩配置
-      new OptimizeCssAssetsPlugin({}),
-      new TerserPlugin({}),
-    ],
-  },
 
   // cache 持久化缓存(优化)
   cache: {
